b1404eb9b33fa15aa89462b9fa7deaf6a6a20670
Mathew Sachin <mathew.sachin.git@outlook.com>
2017-02-03 02:00:53 +0530

    Added Expander for Cursor, Clicks, Keystrokes

--- src/Utility/ModernButton.cs
+++ src/Utility/ModernButton.cs
@@ -4,54 +4,16 @@ using System.Windows.Media;
 
 namespace Captura
 {
-    /// <summary>
-    /// Adds icon content to a standard button.
-    /// </summary>
     public class ModernButton : Button
     {
-        /// <summary>
-        /// Identifies the IconData property.
-        /// </summary>
-        public static readonly DependencyProperty IconDataProperty = DependencyProperty.Register("IconData", typeof(Geometry), typeof(ModernButton));
-        /// <summary>
-        /// Identifies the IconHeight property.
-        /// </summary>
-        public static readonly DependencyProperty IconHeightProperty = DependencyProperty.Register("IconHeight", typeof(double), typeof(ModernButton), new PropertyMetadata(12D));
-        /// <summary>
-        /// Identifies the IconWidth property.
-        /// </summary>
-        public static readonly DependencyProperty IconWidthProperty = DependencyProperty.Register("IconWidth", typeof(double), typeof(ModernButton), new PropertyMetadata(12D));
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ModernButton"/> class.
-        /// </summary>
+        public static readonly DependencyProperty IconDataProperty = DependencyProperty.Register(nameof(IconData), typeof(Geometry), typeof(ModernButton));
+     
         public ModernButton() { DefaultStyleKey = typeof(ModernButton); }
-                
-        /// <summary>
-        /// Gets or sets the icon path data.
-        /// </summary>
+        
         public Geometry IconData
         {
             get { return (Geometry)GetValue(IconDataProperty); }
             set { SetValue(IconDataProperty, value); }
-        }
-
-        /// <summary>
-        /// Gets or sets the icon height.
-        /// </summary>
-        public double IconHeight
-        {
-            get { return (double)GetValue(IconHeightProperty); }
-            set { SetValue(IconHeightProperty, value); }
-        }
-
-        /// <summary>
-        /// Gets or sets the icon width.
-        /// </summary>
-        public double IconWidth
-        {
-            get { return (double)GetValue(IconWidthProperty); }
-            set { SetValue(IconWidthProperty, value); }
         }
     }
 }
---
+++ src/Utility/ModernToggleButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Captura
+{
+    public class ModernToggleButton : CheckBox
+    {
+        public static readonly DependencyProperty IconDataProperty = DependencyProperty.Register(nameof(IconData), typeof(Geometry), typeof(ModernToggleButton));
+        
+        public ModernToggleButton() { DefaultStyleKey = typeof(ModernToggleButton); }
+        
+        public Geometry IconData
+        {
+            get { return (Geometry)GetValue(IconDataProperty); }
+            set { SetValue(IconDataProperty, value); }
+        }
+    }
+}
