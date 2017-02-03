e3390cc195b1be3d5a2ce265135e5eca02659ceb
Mathew Sachin <mathew.sachin.git@outlook.com>
2017-02-03 16:58:41 +0530

    Include Cursor defaults to True

--- src/Properties/Settings.Designer.cs
+++ src/Properties/Settings.Designer.cs
@@ -37,7 +37,7 @@ namespace Captura.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool IncludeCursor {
             get {
                 return ((bool)(this["IncludeCursor"]));
