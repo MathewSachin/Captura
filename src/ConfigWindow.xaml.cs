847fc4f4c0a8942f10a9ce1eb92c9bbcf3c1cdcd
Mathew Sachin <mathew.sachin.git@outlook.com>
2017-02-22 21:33:11 +0530

    refactor

--- src/ConfigWindow.xaml.cs
+++ src/ConfigWindow.xaml.cs
@@ -2,9 +2,26 @@
 {
     public partial class ConfigWindow
     {
-        public ConfigWindow()
+        static ConfigWindow _instance;
+        
+        ConfigWindow()
         {
             InitializeComponent();
+
+            Closing += (s, e) =>
+            {
+                Hide();
+                e.Cancel = true;
+            };
+        }
+
+        public static void ShowFocused()
+        {
+            if (_instance == null)
+                _instance = new ConfigWindow();
+
+            _instance.Show();
+            _instance.Focus();
         }
     }
 }
--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -5,20 +5,12 @@ namespace Captura
 {
     public partial class MainWindow
     {
-        ConfigWindow _configWindow;
         Point _moveOrigin;
         
         public MainWindow()
         {
             InitializeComponent();
-
-            _configWindow = new ConfigWindow();
-            _configWindow.Closing += (s, e) =>
-            {
-                _configWindow.Hide();
-                e.Cancel = true;
-            };
-
+            
             HotKey.RegisterAll();
             
             Closed += (s, e) =>
@@ -28,11 +20,7 @@ namespace Captura
             };
         }
 
-        void ConfigButton_Click(object sender, RoutedEventArgs e)
-        {
-            _configWindow.Show();
-            _configWindow.Focus();
-        }
+        void ConfigButton_Click(object sender, RoutedEventArgs e) => ConfigWindow.ShowFocused();
 
         void Grid_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
