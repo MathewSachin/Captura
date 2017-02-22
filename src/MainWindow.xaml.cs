bb38c215e93e387d3271144657eaea0d8dc5f8b7
Mathew Sachin <mathew.sachin.git@outlook.com>
2017-02-22 21:14:17 +0530

    Improve Window Movement

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ namespace Captura
     public partial class MainWindow
     {
         ConfigWindow _configWindow;
+        Point _moveOrigin;
         
         public MainWindow()
         {
@@ -33,10 +34,37 @@ namespace Captura
             _configWindow.Focus();
         }
 
-        void Grid_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e) => DragMove();
+        void Grid_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+
+            e.Handled = true;
+        }
 
         void MinButton_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
 
         void CloseButton_Click(object sender, RoutedEventArgs e) => Close();
+
+        void Window_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (IsMouseCaptured)
+                ReleaseMouseCapture();
+        }
+
+        void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _moveOrigin = e.GetPosition(this);
+            CaptureMouse();
+        }
+
+        void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsMouseCaptured)
+            {
+                var offset = e.GetPosition(this) - _moveOrigin;
+                Left += offset.X;
+                Top += offset.Y;
+            }
+        }        
     }
 }
