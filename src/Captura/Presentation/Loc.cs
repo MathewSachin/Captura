f8595c9a6ac52680e120bf60c932924b1977e782
Mathew Sachin <mathew.sachin.git@outlook.com>
2017-05-14 23:35:08 +0530

    Added WPF Localization Extension

---
+++ src/Captura/Presentation/Loc.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Data;
+
+namespace Captura
+{
+    public class Loc : Binding
+    {
+        public Loc(string Name) : base("[" + Name + "]")
+        {
+            Mode = BindingMode.OneWay;
+            Source = TranslationSource.Instance;
+        }
+    }
+}
---
+++ src/Captura/Presentation/TranslationSource.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace Captura
+{
+    public class TranslationSource : NotifyPropertyChanged
+    {
+        public static TranslationSource Instance { get; } = new TranslationSource();
+        
+        public string this[string Key] => Properties.Resources.ResourceManager.GetString(Key, CurrentCulture);
+
+        CultureInfo currentCulture;
+
+        public CultureInfo CurrentCulture
+        {
+            get { return currentCulture; }
+            set
+            {
+                if (currentCulture != value)
+                {
+                    currentCulture = value;
+
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
