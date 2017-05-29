bc4c3892b49aab68ac9f1046d5d56a0612f2662e
Mathew Sachin <mathew.sachin.git@outlook.com>
2017-05-29 21:10:46 +0530

    Added CommandLineParser NuGet package

---
+++ src/Captura.Core/CommandLineOptions.cs
@@ -0,0 +1,25 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace Captura
+{
+    public class CommandLineOptions
+    {
+        //[Option('r', "read", Required = true, HelpText = "Input file to be processed.")]
+        //public string InputFile { get; set; }
+
+        // Reset settings
+        // omitting long name, default --reset
+        [Option(DefaultValue = false, HelpText = "Prints all messages to standard output.")]
+        public bool Reset { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this, current => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
