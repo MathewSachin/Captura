03e6f03585b66f48680c5c60b12c3b42c98dbee0
Mathew Sachin <mathew.sachin.git@outlook.com>
2017-06-09 09:22:48 +0530

    update

--- appveyor/Update-ScreenShots.ps1
+++ appveyor/Update-ScreenShots.ps1
@@ -9,14 +9,14 @@ if ($env:configuration -eq 'Release')# -and $env:appveyor_repo_tag -eq 'true')
     # Stage
     git add img/ScreenShots/Tabs
 
-    # Commit
-    git commit -m "Update ScreenShots to $env:APPVEYOR_REPO_TAG_NAME"
-
     # Setup git credentials
     git config --global credential.helper store
     Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"
     git config --global user.email "mathew.sachin.git@outlook.com"
     git config --global user.name "Mathew Sachin"
+
+    # Commit
+    git commit -m "Update ScreenShots to $env:APPVEYOR_REPO_TAG_NAME"
 
     # Push back changes
     git push
