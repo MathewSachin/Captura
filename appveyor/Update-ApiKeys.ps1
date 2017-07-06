d5174fa374104a6b2b592b12cb6ec807458e5fa9
Mathew Sachin <mathew.sachin.git@outlook.com>
2017-07-06 23:26:40 +0530

    Don't Update-ApiKeys on Pull Requests
    Running Update-ApiKeys on Pull Request builds causes failure due to usage of secure environment variable.

--- appveyor/Update-ApiKeys.ps1
+++ appveyor/Update-ApiKeys.ps1
@@ -1,1 +1,5 @@
-Set-Content src/Captura.Core/ApiKeys.cs $env:api_keys
+# Running this script on Pull Requests causes failure due to usage of secure environment variable
+if (-not $env:APPVEYOR_PULL_REQUEST_NUMBER)
+{
+    Set-Content src/Captura.Core/ApiKeys.cs $env:api_keys
+}
