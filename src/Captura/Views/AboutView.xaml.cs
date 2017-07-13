9a2f35ecc4bdf003f5ed141fe16c734a12a6fefc
Mathew Sachin <mathew.sachin.git@outlook.com>
2017-07-13 12:58:51 +0530

    Extras on Config tab

--- src/Captura/Views/AboutView.xaml.cs
+++ src/Captura/Views/AboutView.xaml.cs
@@ -4,11 +4,6 @@ namespace Captura
 {
     public partial class AboutView
     {
-        void OpenExtras(object sender, RoutedEventArgs e)
-        {
-            MoreOptionsWindow.Instance.ShowAndFocus();
-        }
-
         void OpenFFMpegLog(object sender, RoutedEventArgs e)
         {
             FFMpegLogView.Instance.ShowAndFocus();
---
+++ src/Captura/Views/ConfigView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Captura
+{
+    public partial class ConfigView
+    {
+        void OpenExtras(object sender, RoutedEventArgs e)
+        {
+            MoreOptionsWindow.Instance.ShowAndFocus();
+        }
+    }
+}
