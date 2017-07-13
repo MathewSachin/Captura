9f90ebf99d9db464e5c950402f0312daf700e29e
Mathew Sachin <mathew.sachin.git@outlook.com>
2017-07-13 22:34:05 +0530

    NumberBoxes for specifying size of RegionSelector

--- src/Captura/Presentation/Controls/RegionSelector.xaml.cs
+++ src/Captura/Presentation/Controls/RegionSelector.xaml.cs
@@ -62,6 +62,47 @@ namespace Captura
 
             // Prevent Closing by User
             Closing += (s, e) => e.Cancel = true;
+
+            #region Height and Width Boxes
+            WidthBox.Minimum = (int)MinWidth - WidthBorder;
+            HeightBox.Minimum = (int)MinHeight - HeightBorder;
+
+            void sizeChange()
+            {
+                var selectedRegion = SelectedRegion;
+
+                WidthBox.Value = selectedRegion.Width;
+                HeightBox.Value = selectedRegion.Height;
+            }
+
+            SizeChanged += (s, e) => sizeChange();
+
+            sizeChange();
+            
+            WidthBox.ValueChanged += (s, e) =>
+            {
+                if (e.NewValue == null)
+                    return;
+
+                var selectedRegion = SelectedRegion;
+
+                selectedRegion.Width = WidthBox.Value.Value;
+
+                SelectedRegion = selectedRegion;
+            };
+
+            HeightBox.ValueChanged += (s, e) =>
+            {
+                if (e.NewValue == null)
+                    return;
+
+                var selectedRegion = SelectedRegion;
+
+                selectedRegion.Height = HeightBox.Value.Value;
+
+                SelectedRegion = selectedRegion;
+            };
+            #endregion
         }
 
         void CloseButton_Click(object sender, RoutedEventArgs e)
@@ -142,10 +183,15 @@ namespace Captura
             }
         }
 
+        const int LeftOffset = 7,
+            TopOffset = 37,
+            WidthBorder = 14,
+            HeightBorder = 74;
+
         // Ignoring Borders and Header
         public Rectangle SelectedRegion
         {
-            get => Dispatcher.Invoke(() => new Rectangle((int)Left + 7, (int)Top + 37, (int)Width - 14, (int)Height - 44)) * Dpi.Instance;
+            get => Dispatcher.Invoke(() => new Rectangle((int)Left + LeftOffset, (int)Top + TopOffset, (int)Width - WidthBorder, (int)Height - HeightBorder)) * Dpi.Instance;
             set
             {
                 if (value == Rectangle.Empty)
@@ -156,11 +202,11 @@ namespace Captura
 
                 Dispatcher.Invoke(() =>
                 {
-                    Left = value.Left - 7;
-                    Top = value.Top - 37;
+                    Left = value.Left - LeftOffset;
+                    Top = value.Top - TopOffset;
 
-                    Width = value.Width + 14;
-                    Height = value.Height + 44;
+                    Width = value.Width + WidthBorder;
+                    Height = value.Height + HeightBorder;
 
                     // Prevent going off-screen
                     if (Left < 0)
