2b7cc4a9f57249bf02a17976a71ecf6c62052c0e
Mathew Sachin <mathew.sachin.git@outlook.com>
2017-07-17 16:14:22 +0530

    init_hw_device not required

--- src/Captura.Core/Models/VideoWriterItems/FFMpegItem.cs
+++ src/Captura.Core/Models/VideoWriterItems/FFMpegItem.cs
@@ -28,7 +28,7 @@ namespace Captura.Models
 
         public static FFMpegVideoArgsProvider HEVC_QSV { get; } = VideoQuality =>
         {
-            return new FFMpegVideoArgs("-init_hw_device qsv", $"-vcodec hevc_qsv -load_plugin hevc_hw -q 2 -preset:v veryfast");
+            return new FFMpegVideoArgs("", $"-vcodec hevc_qsv -load_plugin hevc_hw -q 2 -preset:v veryfast");
         };
 
         public static FFMpegItem[] Items { get; } =
