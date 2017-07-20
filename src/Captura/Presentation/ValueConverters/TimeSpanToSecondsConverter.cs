50e8c33720ac1adcc26492fbb69c5574eedc96bf
Mathew Sachin <mathew.sachin.git@outlook.com>
2017-07-20 16:49:23 +0530

    AudioEditor still not done

---
+++ src/Captura/Presentation/ValueConverters/TimeSpanToSecondsConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace Captura
+{
+    public class TimeSpanToSecondsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TimeSpan t)
+                return t.TotalSeconds;
+
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double seconds)
+                return TimeSpan.FromSeconds((int)seconds);
+
+            return Binding.DoNothing;
+        }
+    }
+}
--- src/Captura/Views/AudioEditor.xaml.cs
+++ src/Captura/Views/AudioEditor.xaml.cs
@@ -1,97 +1,15 @@
-﻿using Captura.ViewModels;
-using System;
-using System.Diagnostics;
-using System.Windows.Media;
-
-namespace Captura
+﻿namespace Captura
 {
-    public class AudioEditorViewModel : ViewModelBase
-    {
-        TimeSpan _begin, _end, _current, _duration;
-        string _fileName;
-
-        MediaPlayer _player;
-
-        public TimeSpan Begin
-        {
-            get { return _begin; }
-            set
-            {
-                _begin = value;
-
-                OnPropertyChanged();
-            }
-        }
-
-        public TimeSpan End
-        {
-            get { return _end; }
-            set
-            {
-                _end = value;
-
-                OnPropertyChanged();
-            }
-        }
-
-        public TimeSpan Current
-        {
-            get { return _current; }
-            set
-            {
-                _current = value;
-
-                if (_current > End || _current < Begin)
-                    _current = Begin;
-
-                _player.Position = _current;
-
-                OnPropertyChanged();
-            }
-        }
-        
-        public TimeSpan Duration
-        {
-            get { return _duration; }
-            set
-            {
-                _duration = value;
-
-                OnPropertyChanged();
-            }
-        }
-
-        public void Load(string FileName)
-        {
-            _fileName = FileName;
-            _player = new MediaPlayer();
-            
-            _player.Open(new Uri(FileName));
-
-            Begin = Current = TimeSpan.Zero;
-            End = Duration = _player.NaturalDuration.TimeSpan;            
-        }
-
-        public void Save()
-        {
-            Process.Start(FFMpegService.FFMpegExePath, $"-y -ss {_begin.TotalSeconds} -i {_fileName} -to {_end.TotalSeconds} -acodec copy {_fileName}");
-        }
-
-        public void Reset()
-        {
-            _player.Stop();
-
-            _player.Position = _begin = TimeSpan.Zero;
-
-            _end = _duration;
-        }
-    }
-
     public partial class AudioEditor
     {
         public AudioEditor()
         {
             InitializeComponent();
         }        
+
+        public void Load(string FileName)
+        {
+            (DataContext as AudioEditorViewModel).Load(FileName);
+        }
     }
 }
