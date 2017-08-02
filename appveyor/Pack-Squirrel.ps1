4d86ce14c5dc40b233d6f6bc9519503f753c39b0
Mathew Sachin <mathew.sachin.git@outlook.com>
2017-08-02 23:49:39 +0530

    t

--- appveyor/Pack-Squirrel.ps1
+++ appveyor/Pack-Squirrel.ps1
@@ -1,6 +1,6 @@
 if ($env:configuration -eq 'Release' -and $env:appveyor_repo_tag -eq 'true')
 {
-    nuget pack "src/Captura.nuspec" --version $env:TagVersion
+    nuget pack "src/Captura.nuspec" -version $env:TagVersion
 
     . (gci "src/packages/Squirrel.Windows**/Squirrel.exe" -recurse)[0].FullName --releasify "Captura.$env:TagVersion.nupkg" --no-msi
 }
