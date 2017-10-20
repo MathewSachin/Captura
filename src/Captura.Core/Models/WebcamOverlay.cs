2275f4f2de25d0e3d6afcf42bc35262736489d97
Mathew Sachin <mathew.sachin.git@outlook.com>
2017-10-20 21:10:25 +0530

    Webcam overlay position config

--- src/Captura.Core/Models/WebcamOverlay.cs
+++ src/Captura.Core/Models/WebcamOverlay.cs
@@ -8,6 +8,35 @@ namespace Captura.Models
     {
         public void Dispose() { }
 
+        static Point GetPosition(Size Bounds, Size ImageSize)
+        {
+            var point = new Point(Settings.Instance.Webcam_X, Settings.Instance.Webcam_Y);
+
+            switch (Settings.Instance.Webcam_XAlign)
+            {
+                case Alignment.Center:
+                    point.X = Bounds.Width / 2 - ImageSize.Width / 2 + point.X;
+                    break;
+
+                case Alignment.End:
+                    point.X = Bounds.Width - ImageSize.Width - point.X;
+                    break;
+            }
+
+            switch (Settings.Instance.Webcam_YAlign)
+            {
+                case Alignment.Center:
+                    point.Y = Bounds.Height / 2 - ImageSize.Height / 2 + point.Y;
+                    break;
+
+                case Alignment.End:
+                    point.Y = Bounds.Height - ImageSize.Height - point.Y;
+                    break;
+            }
+
+            return point;
+        }
+
         public void Draw(Graphics g, Func<Point, Point> PointTransform = null)
         {
             var webcam = ServiceProvider.WebCamProvider;
@@ -18,12 +47,15 @@ namespace Captura.Models
 
                 if (img == null)
                     return;
-
-                var point = new Point((int)g.VisibleClipBounds.Width - img.Width - 10, (int)g.VisibleClipBounds.Height - img.Height - 10);
-
+                
                 using (img)
                 {
-                    try { g.DrawImage(img, point); }
+                    try
+                    {
+                        var point = GetPosition(new Size((int)g.VisibleClipBounds.Width, (int)g.VisibleClipBounds.Height), img.Size);
+
+                        g.DrawImage(img, point);
+                    }
                     catch { }
                 }
             }
---
+++ src/Captura.Core/Settings/Webcam.cs
@@ -0,0 +1,41 @@
+﻿using System.Configuration;
+using System.Drawing;
+// ReSharper disable UnusedMember.Global
+
+namespace Captura
+{
+    public partial class Settings
+    {   
+        [UserScopedSetting]
+        [DefaultSettingValue(nameof(Alignment.End))]
+        public Alignment Webcam_XAlign
+        {
+            get => Get<Alignment>();
+            set => Set(value);
+        }
+
+        [UserScopedSetting]
+        [DefaultSettingValue(nameof(Alignment.End))]
+        public Alignment Webcam_YAlign
+        {
+            get => Get<Alignment>();
+            set => Set(value);
+        }
+
+        [UserScopedSetting]
+        [DefaultSettingValue("20")]
+        public int Webcam_X
+        {
+            get => Get<int>();
+            set => Set(value);
+        }
+
+        [UserScopedSetting]
+        [DefaultSettingValue("20")]
+        public int Webcam_Y
+        {
+            get => Get<int>();
+            set => Set(value);
+        }
+    }
+}
