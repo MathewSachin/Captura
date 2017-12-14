4a76cc033efb19c8982230c5224b619fb0e526b2
Mathew Sachin <mathew.sachin.git@outlook.com>
2017-12-14 08:48:07 +0530

    Refactor

--- src/Captura.Core/Settings/Providers/ArraySettingsProvider.cs
+++ src/Captura.Core/Settings/Providers/ArraySettingsProvider.cs
@@ -1,5 +1,4 @@
 ﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 using System.Configuration;
 using System.IO;
 
--- src/Captura.Core/Settings/Providers/JsonSettingsProvider.cs
+++ src/Captura.Core/Settings/Providers/JsonSettingsProvider.cs
@@ -1,10 +1,7 @@
-﻿using System.Collections.Generic;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 using System.Configuration;
 using System.IO;
 using System.Linq;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 
 namespace Captura
 {
