7a180162a120885816bc76bbb7202f90ff4b22d9
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-01-20 17:20:02 +0530

    Main Module

---
+++ src/Captura/Models/MainModule.cs
@@ -0,0 +1,17 @@
+﻿using Captura.Models;
+using Ninject.Modules;
+
+namespace Captura
+{
+    public class MainModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IMessageProvider>().To<MessageProvider>().InSingletonScope();
+            Bind<IRegionProvider>().To<RegionSelector>().InSingletonScope();
+            Bind<ISystemTray>().To<SystemTray>().InSingletonScope();
+            Bind<IWebCamProvider>().To<WebCamProvider>().InSingletonScope();
+            Bind<IMainWindow>().To<MainWindowProvider>().InSingletonScope();
+        }
+    }
+}
