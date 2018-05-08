674b1645198740cc9ffe34528b45a5b8928ccff4
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-05-08 17:40:29 +0530

    Partial work for Preview while writing to file

---
+++ src/Captura/Models/WithPreviewWriter.cs
@@ -0,0 +1,43 @@
+﻿using Screna;
+
+namespace Captura.Models
+{
+    public class WithPreviewWriter : IVideoFileWriter
+    {
+        readonly PreviewWriter _previewWriter = new PreviewWriter();
+        readonly IVideoFileWriter _writer;
+
+        public WithPreviewWriter(IVideoFileWriter Writer)
+        {
+            _writer = Writer;
+        }
+
+        public void Dispose()
+        {
+            _writer.Dispose();
+            _previewWriter.Dispose();
+        }
+
+        public void WriteFrame(IBitmapFrame Image)
+        {
+            if (Image is RepeatFrame)
+            {
+                _writer.WriteFrame(Image);
+            }
+            else
+            {
+                var frame = new MultiDisposeFrame(Image, 2);
+
+                _previewWriter.WriteFrame(frame);
+                _writer.WriteFrame(frame);
+            }
+        }
+
+        public bool SupportsAudio => _writer.SupportsAudio;
+
+        public void WriteAudio(byte[] Buffer, int Length)
+        {
+            _writer.WriteAudio(Buffer, Length);
+        }
+    }
+}
---
+++ src/Screna/Frames/MultiDisposeFrame.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+
+namespace Screna
+{
+    public class MultiDisposeFrame : IBitmapFrame
+    {
+        int _count;
+        readonly IBitmapFrame _frame;
+        readonly object _syncLock = new object();
+
+        public MultiDisposeFrame(IBitmapFrame Frame, int Count)
+        {
+            if (_frame is RepeatFrame)
+            {
+                throw new NotSupportedException();
+            }
+
+            if (Count < 2)
+            {
+                throw new ArgumentException("Count should be atleast 2", nameof(Count));
+            }
+
+            _frame = Frame ?? throw new ArgumentNullException(nameof(Frame));
+            _count = Count;
+        }
+
+        public void Dispose()
+        {
+            lock (_syncLock)
+            {
+                --_count;
+
+                if (_count == 0)
+                {
+                    _frame.Dispose();
+                }
+            }
+        }
+
+        public Bitmap Bitmap => _frame.Bitmap;
+
+        public IBitmapEditor GetEditor() => _frame.GetEditor();
+    }
+}
