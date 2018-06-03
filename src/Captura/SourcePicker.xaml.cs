be261f2ed1dfb24dde8d766b622c3e2e967f8595
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-06-03 18:25:12 +0530

    begin

---
+++ src/Captura/SourcePicker.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Captura
+{
+    public partial class SourcePicker
+    {
+        public SourcePicker()
+        {
+            InitializeComponent();
+        }
+
+        void CloseClick(object Sender, RoutedEventArgs E)
+        {
+            Close();
+        }
+    }
+}
