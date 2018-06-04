4c95c0ddbf22803fed288814889e794ee0e4f27e
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-06-04 15:44:05 +0530

    t

--- src/Captura/SourcePicker.xaml.cs
+++ src/Captura/SourcePicker.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System.Drawing.Imaging;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Screna;
 
 namespace Captura
 {
@@ -7,6 +12,22 @@ namespace Captura
         public SourcePicker()
         {
             InitializeComponent();
+
+            UpdateBackground();
+        }
+
+        void UpdateBackground()
+        {
+            using (var bmp = ScreenShot.Capture())
+            {
+                var stream = new MemoryStream();
+                bmp.Save(stream, ImageFormat.Png);
+
+                stream.Seek(0, SeekOrigin.Begin);
+
+                var decoder = new PngBitmapDecoder(stream, BitmapCreateOptions.None, BitmapCacheOption.Default);
+                Background = new ImageBrush(decoder.Frames[0]);
+            }
         }
 
         void CloseClick(object Sender, RoutedEventArgs E)
