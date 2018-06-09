54590417958efd25750ed5b1cb13e275c8b946d9
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-06-09 22:50:29 +0530

    separate window

--- src/Captura/Views/HotkeyView.xaml.cs
+++ src/Captura/Views/HotkeyView.xaml.cs
@@ -1,10 +1,18 @@
-﻿namespace Captura
+﻿using System.Windows;
+using Captura.Views;
+
+namespace Captura
 {
     public partial class HotkeyView
     {
         public HotkeyView()
         {
             InitializeComponent();
+        }
+
+        void OpenHotkeyManager(object Sender, RoutedEventArgs E)
+        {
+            new HotkeysWindow().ShowAndFocus();
         }
     }
 }
