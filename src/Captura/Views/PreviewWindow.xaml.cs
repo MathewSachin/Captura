2ba5e7fc6fa7f4c7d02ba7bb07ceccfad66fc97d
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-06-20 23:47:52 +0530

    Preview Window Image Stretch option

--- src/Captura/Views/PreviewWindow.xaml.cs
+++ src/Captura/Views/PreviewWindow.xaml.cs
@@ -1,10 +1,30 @@
-﻿namespace Captura
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Captura
 {
     public partial class PreviewWindow
     {
         public PreviewWindow()
         {
             InitializeComponent();
+
+            StrectValues.ItemsSource = new[]
+            {
+                Stretch.Uniform,
+                Stretch.Fill,
+                Stretch.UniformToFill
+            };
+
+            StrectValues.SelectedIndex = 0;
+        }
+
+        void StrectValues_OnSelectionChanged(object Sender, SelectionChangedEventArgs E)
+        {
+            if (StrectValues.SelectedValue is Stretch stretch)
+            {
+                DisplayImage.Stretch = stretch;
+            }
         }
     }
 }
