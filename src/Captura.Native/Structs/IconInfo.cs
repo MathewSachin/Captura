c422a594457a32bc9851ba94f79cfdab16f4ce4f
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-06-30 09:15:48 +0530

    native

---
+++ src/Captura.Native/Structs/IconInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable FieldCanBeMadeReadOnly.Global
+
+namespace Captura.Native
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct IconInfo
+    {
+        public bool fIcon;
+        public int xHotspot;
+        public int yHotspot;
+        public IntPtr hbmMask;
+        public IntPtr hbmColor;
+    }
+}
---
+++ src/Captura.Native/Structs/RECT.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Captura.Native
+{
+    [Serializable, StructLayout(LayoutKind.Sequential)]
+    // ReSharper disable once InconsistentNaming
+    public struct RECT
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+
+        public RECT(int Dimension)
+        {
+            Left = Top = Right = Bottom = Dimension;
+        }
+    }
+}
