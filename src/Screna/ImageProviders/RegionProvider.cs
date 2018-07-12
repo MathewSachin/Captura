9ebbd82f1583cf127a2e0b749602db75bb9c4be8
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-07-12 10:42:48 +0530

    On Odd sizes increase instead of decrease size in RegionProvider

--- src/Screna/ImageProviders/RegionProvider.cs
+++ src/Screna/ImageProviders/RegionProvider.cs
@@ -15,8 +15,18 @@ namespace Screna
 
         public RegionProvider(Rectangle Region, bool IncludeCursor)
         {
-            _region = Region.Even();
+            _region = Region;
             _includeCursor = IncludeCursor;
+
+            // Width and Height must be even.
+            // Use these for Bitmap size, but capture as per region size
+            Width = _region.Width;
+            if (Width % 2 == 1)
+                ++Width;
+            
+            Height = _region.Height;
+            if (Height % 2 == 1)
+                ++Height;
 
             _transform = P => new Point(P.X - _region.X, P.Y - _region.Y);
 
@@ -45,7 +55,7 @@ namespace Screna
 
         public IBitmapFrame Capture()
         {
-            Gdi32.BitBlt(_hdcDest, 0, 0, Width, Height,
+            Gdi32.BitBlt(_hdcDest, 0, 0, _region.Width, _region.Height,
                 _hdcSrc, _region.X, _region.Y,
                 (int) CopyPixelOperation.SourceCopy);
 
@@ -58,7 +68,7 @@ namespace Screna
             return img;
         }
 
-        public int Height => _region.Height;
-        public int Width => _region.Width;
+        public int Height { get; }
+        public int Width { get; }
     }
 }
