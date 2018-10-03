196426bd0776957aab708842db2c18709ce58eca
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-10-03 10:17:05 +0530

    prototype

---
+++ src/Captura.Base/Audio/IAudioPlayer.cs
@@ -0,0 +1,15 @@
+﻿namespace Captura.Models
+{
+    public interface IAudioPlayer
+    {
+        void PlayBegin();
+
+        void PlayEnd();
+
+        void PlayPause();
+
+        void PlayShot();
+
+        void PlayError();
+    }
+}
---
+++ src/Captura/Models/AudioPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Media;
+
+namespace Captura.Models
+{
+    public class AudioPlayer : IAudioPlayer
+    {
+        public void PlayBegin()
+        {
+            SystemSounds.Beep.Play();
+        }
+
+        public void PlayEnd()
+        {
+            SystemSounds.Beep.Play();
+        }
+
+        public void PlayPause()
+        {
+            SystemSounds.Beep.Play();
+        }
+
+        public void PlayShot()
+        {
+            SystemSounds.Beep.Play();
+        }
+
+        public void PlayError()
+        {
+            SystemSounds.Asterisk.Play();
+        }
+    }
+}
