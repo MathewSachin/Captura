f7bf21e88631e7872b266df52b19f9cfaa5476df
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-10-07 21:15:41 +0530

    reset btns

--- src/Captura.Core/ViewModels/SoundsViewModel.cs
+++ src/Captura.Core/ViewModels/SoundsViewModel.cs
@@ -1,7 +1,9 @@
-﻿using Captura.Models;
+﻿using System.Windows.Input;
+using Captura.Models;
 
 namespace Captura.ViewModels
 {
+    // ReSharper disable once ClassNeverInstantiated.Global
     public class SoundsViewModel : NotifyPropertyChanged
     {
         readonly IDialogService _dialogService;
@@ -12,6 +14,16 @@ namespace Captura.ViewModels
         {
             _dialogService = DialogService;
             this.Settings = Settings;
+
+            ResetNormalCommand = new DelegateCommand(() => Settings.Normal = null);
+            ResetShotCommand = new DelegateCommand(() => Settings.Shot = null);
+            ResetErrorCommand = new DelegateCommand(() => Settings.Error = null);
+            ResetNotificationCommand = new DelegateCommand(() => Settings.Notification = null);
         }
+
+        public ICommand ResetNormalCommand { get; }
+        public ICommand ResetShotCommand { get; }
+        public ICommand ResetErrorCommand { get; }
+        public ICommand ResetNotificationCommand { get; }
     }
 }
