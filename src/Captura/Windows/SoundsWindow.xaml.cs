c6fe6529294050a376ff8dfc70665d4231035820
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-10-07 22:04:15 +0530

    t

--- src/Captura/Windows/SoundsWindow.xaml.cs
+++ src/Captura/Windows/SoundsWindow.xaml.cs
@@ -1,10 +1,45 @@
-﻿namespace Captura
+﻿using System.Windows.Input;
+using Captura.ViewModels;
+
+namespace Captura
 {
     public partial class SoundsWindow
     {
         public SoundsWindow()
         {
             InitializeComponent();
+        }
+
+        void SetNormalFile(object Sender, MouseButtonEventArgs E)
+        {
+            if (DataContext is SoundsViewModel vm)
+            {
+                vm.SetNormalCommand.ExecuteIfCan();
+            }
+        }
+
+        void SetShotFile(object Sender, MouseButtonEventArgs E)
+        {
+            if (DataContext is SoundsViewModel vm)
+            {
+                vm.SetShotCommand.ExecuteIfCan();
+            }
+        }
+
+        void SetErrorFile(object Sender, MouseButtonEventArgs E)
+        {
+            if (DataContext is SoundsViewModel vm)
+            {
+                vm.SetErrorCommand.ExecuteIfCan();
+            }
+        }
+
+        void SetNotificationFile(object Sender, MouseButtonEventArgs E)
+        {
+            if (DataContext is SoundsViewModel vm)
+            {
+                vm.SetNotificationCommand.ExecuteIfCan();
+            }
         }
     }
 }
