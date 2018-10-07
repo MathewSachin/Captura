bd9d3578f1cccdc4a951cf35eacca6f71719783d
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-10-07 21:03:25 +0530

    add soundsviewodel

---
+++ src/Captura.Core/ViewModels/SoundsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Captura.Models;
+
+namespace Captura.ViewModels
+{
+    public class SoundsViewModel : NotifyPropertyChanged
+    {
+        readonly IDialogService _dialogService;
+
+        public SoundSettings Settings { get; }
+
+        public SoundsViewModel(IDialogService DialogService, SoundSettings Settings)
+        {
+            _dialogService = DialogService;
+            this.Settings = Settings;
+        }
+    }
+}
