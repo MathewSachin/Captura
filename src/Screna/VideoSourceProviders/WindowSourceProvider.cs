1edee96e6206268c0104646b04752c93a6283b79
Hyunjong Lee <hyunjong.lee.s@gmail.com>
2018-10-09 22:36:22 +0900

    make window source working in console mode

--- src/Screna/VideoSourceProviders/WindowSourceProvider.cs
+++ src/Screna/VideoSourceProviders/WindowSourceProvider.cs
@@ -17,12 +17,27 @@ namespace Captura.Models
 
         public bool PickWindow()
         {
-            var window = _videoSourcePicker.PickWindow(new [] { _regionProvider.Handle });
+            if (Source != null)
+            {
+                var winItem = (WindowItem)Source;
+                if (winItem != null)
+                {
+                    _source = new WindowItem(new Window(winItem.Window.Handle));
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                var window = _videoSourcePicker.PickWindow(new[] { _regionProvider.Handle });
 
-            if (window == null)
-                return false;
+                if (window == null)
+                    return false;
 
-            _source = new WindowItem(new Window(window.Handle));
+                _source = new WindowItem(new Window(window.Handle));
+            }
 
             RaisePropertyChanged(nameof(Source));
             return true;
