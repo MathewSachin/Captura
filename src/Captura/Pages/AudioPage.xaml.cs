08a86e327ba352987b08088741e03f786d70b215
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-10-22 12:27:03 +0530

    t

--- src/Captura/Pages/AudioPage.xaml.cs
+++ src/Captura/Pages/AudioPage.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace Captura
+﻿using Captura.ViewModels;
+
+namespace Captura
 {
     public partial class AudioPage
     {
         public AudioPage()
         {
             InitializeComponent();
+
+            ServiceProvider.Get<MainViewModel>().Refreshed += () =>
+            {
+                AudioSourcesPanel.Shake();
+            };
         }
     }
 }
