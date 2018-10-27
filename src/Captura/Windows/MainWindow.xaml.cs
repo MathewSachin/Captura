793d3c72842b4597e0d0bc8604341b6bc962454c
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-10-27 15:45:12 +0530

    tray show mainwindow on left click

--- src/Captura/Windows/MainWindow.xaml.cs
+++ src/Captura/Windows/MainWindow.xaml.cs
@@ -149,5 +149,15 @@ namespace Captura
         void MenuExit_Click(object Sender, RoutedEventArgs Args) => Close();
 
         void HideButton_Click(object Sender, RoutedEventArgs Args) => Hide();
+
+        void ShowMainWindow(object Sender, RoutedEventArgs E)
+        {
+            this.ShowAndFocus();
+        }
+
+        void TrayLeftClick(object Sender, ExecutedRoutedEventArgs E)
+        {
+            this.ShowAndFocus();
+        }
     }
 }
