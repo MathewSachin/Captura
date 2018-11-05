a091cedacb9d43b2ddb38e7997558e7a4993a184
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-11-05 14:31:13 +0530

    Added ShFileOperation

---
+++ src/Captura.Native/Structs/ShFileOpStruct.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+// ReSharper disable FieldCanBeMadeReadOnly.Global
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace Captura.Native
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct ShFileOpStruct
+    {
+        public IntPtr hwnd;
+
+        public FileOperationType Func;
+
+        [MarshalAs(UnmanagedType.LPWStr)]
+        public string From;
+
+        [MarshalAs(UnmanagedType.LPWStr)]
+        public string To;
+
+        public FileOperationFlags Flags;
+
+        [MarshalAs(UnmanagedType.Bool)]
+        public bool AnyOperationsAborted;
+
+        public IntPtr NameMappings;
+
+        [MarshalAs(UnmanagedType.LPWStr)]
+        public string ProgressTitle;
+    }
+}
