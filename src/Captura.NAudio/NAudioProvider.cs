d51b24f6b9c08c384b74ca2110a19c72577633ca
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-11-17 21:42:10 +0530

    NAudioProvider

---
+++ src/Captura.NAudio/NAudioProvider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Captura.Audio;
+using NAudio.Wave;
+using WaveFormat = Captura.Audio.WaveFormat;
+
+namespace Captura.NAudio
+{
+    public abstract class NAudioProvider : IAudioProvider
+    {
+        readonly IWaveIn _waveIn;
+
+        protected NAudioProvider(IWaveIn WaveIn)
+        {
+            _waveIn = WaveIn;
+
+            _waveIn.DataAvailable += (S, E) =>
+            {
+                DataAvailable?.Invoke(this, new DataAvailableEventArgs(E.Buffer, E.BytesRecorded));
+            };
+        }
+
+        public void Dispose()
+        {
+            _waveIn.Dispose();
+        }
+
+        public WaveFormat WaveFormat { get; }
+
+        public void Start()
+        {
+            _waveIn.StartRecording();
+        }
+
+        public void Stop()
+        {
+            _waveIn.StopRecording();
+        }
+
+        public event EventHandler<DataAvailableEventArgs> DataAvailable;
+    }
+}
---
+++ src/Captura.NAudio/WasapiLoopbackCaptureProvider.cs
@@ -0,0 +1,11 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Wave;
+
+namespace Captura.NAudio
+{
+    public class WasapiLoopbackCaptureProvider : NAudioProvider
+    {
+        public WasapiLoopbackCaptureProvider(MMDevice Device)
+            : base(new WasapiLoopbackCapture(Device)) { }
+    }
+}
