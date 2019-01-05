2589904728de2dd8ec04424aba4f5ade6dec6ff9
Mathew Sachin <mathew.sachin.git@outlook.com>
2019-01-06 00:28:02 +0530

    DrawingFrameBase fix with PreviewOnly (non parallel copy)

--- src/Screna/Frames/DrawingFrameBase.cs
+++ src/Screna/Frames/DrawingFrameBase.cs
@@ -33,12 +33,7 @@ namespace Screna
         {
             var bits = Bitmap.LockBits(new Rectangle(Point.Empty, Bitmap.Size), ImageLockMode.ReadOnly, PixelFormat.Format32bppRgb);
 
-            Parallel.For(0, Height, Y =>
-            {
-                var absStride = Math.Abs(bits.Stride);
-
-                Marshal.Copy(bits.Scan0 + Y * bits.Stride, Buffer, Y * absStride, absStride);
-            });
+            Marshal.Copy(bits.Scan0, Buffer, 0, Length);
 
             Bitmap.UnlockBits(bits);
         }
