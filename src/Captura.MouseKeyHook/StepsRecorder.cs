a02e285d83cae1a0008ab1c2507a5285f83a62dd
Mathew Sachin <mathew.sachin.git@outlook.com>
2019-04-04 17:09:30 +0530

    Reactive implementation for SetupRecorder

--- src/Captura.MouseKeyHook/StepsRecorder.cs
+++ src/Captura.MouseKeyHook/StepsRecorder.cs
@@ -1,23 +1,46 @@
 ﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Threading;
 using System.Threading.Tasks;
-
-// ReSharper disable MethodSupportsCancellation
 
 namespace Captura.Models
 {
     public class StepsRecorder : IRecorder
     {
+        readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
         readonly IMouseKeyHook _hook;
         IVideoFileWriter _videoWriter;
         IImageProvider _imageProvider;
-
-        readonly AutoResetEvent _captureEvent;
-        readonly ManualResetEvent _continueCapturing;
-        readonly CancellationTokenSource _cancellationTokenSource;
-        readonly CancellationToken _cancellationToken;
-
         readonly Task _recordTask;
+        volatile bool _recording;
+
+        IObservable<Unit> Observe(IMouseKeyHook Hook, CancellationToken CancellationToken)
+        {
+            var subject = new Subject<Unit>();
+
+            void Callback<T>(object S, T Args) => subject.OnNext(Unit.Default);
+
+            Hook.KeyUp += Callback;
+            Hook.KeyDown += Callback;
+            Hook.MouseUp += Callback;
+            Hook.MouseDown += Callback;
+
+            CancellationToken.Register(() =>
+            {
+                Hook.KeyUp -= Callback;
+                Hook.KeyDown -= Callback;
+                Hook.MouseUp -= Callback;
+                Hook.MouseDown -= Callback;
+
+                subject.OnCompleted();
+            });
+
+            return subject
+                .Where(M => _recording)
+                .Throttle(TimeSpan.FromMilliseconds(70));
+        }
 
         public StepsRecorder(IMouseKeyHook Hook,
             IVideoFileWriter VideoWriter,
@@ -27,44 +50,14 @@ namespace Captura.Models
             _videoWriter = VideoWriter;
             _imageProvider = ImageProvider;
 
-            _cancellationTokenSource = new CancellationTokenSource();
-            _cancellationToken = _cancellationTokenSource.Token;
-
-            _captureEvent = new AutoResetEvent(true);
-            _continueCapturing = new ManualResetEvent(false);
-
-            Hook.KeyUp += HookCallback;
-            Hook.KeyDown += HookCallback;
-            Hook.MouseUp += HookCallback;
-            Hook.MouseDown += HookCallback;
-
             _recordTask = Task.Factory.StartNew(DoRecord, TaskCreationOptions.LongRunning);
-        }
-
-        void HookCallback<T>(object Sender, T Args)
-        {
-            _captureEvent.Set();
         }
 
         void DoRecord()
         {
-            bool CanContinue()
-            {
-                try
-                {
-                    return WaitHandle.WaitAll(new WaitHandle[]
-                    {
-                        _continueCapturing,
-                        _captureEvent
-                    });
-                }
-                catch (ObjectDisposedException)
-                {
-                    return false;
-                }
-            }
+            var observer = Observe(_hook, _cancellationTokenSource.Token);
 
-            while (CanContinue() && !_cancellationToken.IsCancellationRequested)
+            foreach (var _ in observer.ToEnumerable())
             {
                 var editableFrame = _imageProvider.Capture();
 
@@ -81,34 +74,23 @@ namespace Captura.Models
             }
         }
 
-        public void Start()
-        {
-            _continueCapturing?.Set();
-        }
+        public void Start() => _recording = true;
 
-        public void Stop()
-        {
-            _continueCapturing?.Reset();
-        }
+        public void Stop() => _recording = false;
 
         public event Action<Exception> ErrorOccurred;
 
         public void Dispose()
         {
+            _recording = false;
+
             _hook.Dispose();
             _cancellationTokenSource.Cancel();
-
-            // Resume record loop if paused so it can exit
-            _continueCapturing.Set();
-            _captureEvent.Set();
 
             _recordTask.Wait();
 
             _videoWriter.Dispose();
             _videoWriter = null;
-
-            _continueCapturing.Dispose();
-            _captureEvent.Dispose();
 
             _imageProvider?.Dispose();
             _imageProvider = null;
