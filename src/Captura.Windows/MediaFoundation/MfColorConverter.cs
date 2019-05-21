009d02f0a64c4793d8753ad7be9d9d76ea96fe5b
Mathew Sachin <mathew.sachin.git@outlook.com>
2019-05-21 19:45:32 +0530

    finally!!

--- src/Captura.Windows/MediaFoundation/MfColorConverter.cs
+++ src/Captura.Windows/MediaFoundation/MfColorConverter.cs
@@ -1,4 +1,5 @@
 ﻿using DesktopDuplication;
+using MediaFoundation.Transform;
 using SharpDX.Direct3D11;
 using SharpDX.DXGI;
 using SharpDX.MediaFoundation;
@@ -21,8 +22,7 @@ namespace Captura
             _deviceMan = new DXGIDeviceManager();
             _deviceMan.ResetDevice(Device);
 
-            // TODO: Works without this line.
-            //_colorConverter.ProcessMessage(TMessageType.SetD3DManager, _deviceMan.NativePointer);
+            _colorConverter.ProcessMessage(TMessageType.SetD3DManager, _deviceMan.NativePointer);
 
             using (var mediaTypeIn = new MediaType())
             {
@@ -51,10 +51,12 @@ namespace Captura
                 _colorConverter.SetOutputType(0, mediaTypeOut, 0);
             }
 
+            _colorConverter.ProcessMessage(TMessageType.NotifyBeginStreaming, IntPtr.Zero);
+
             _copyTexture = new Texture2D(Device, new Texture2DDescription
             {
-                CpuAccessFlags = CpuAccessFlags.Read,
-                BindFlags = BindFlags.None,
+                CpuAccessFlags = CpuAccessFlags.None,
+                BindFlags = BindFlags.RenderTarget | BindFlags.ShaderResource,
                 Format = Format.B8G8R8A8_UNorm,
                 Width = Width,
                 Height = Height,
@@ -62,7 +64,7 @@ namespace Captura
                 MipLevels = 1,
                 ArraySize = 1,
                 SampleDescription = { Count = 1, Quality = 0 },
-                Usage = ResourceUsage.Staging
+                Usage = ResourceUsage.Default
             });
 
             _inputSample = MediaFactory.CreateVideoSampleFromSurface(null);
@@ -75,75 +77,47 @@ namespace Captura
 
             // Attach the created buffer to the sample
             _inputSample.AddBuffer(inputBuffer);
-
-            _outputTexture = new Texture2D(Device, new Texture2DDescription
-            {
-                CpuAccessFlags = CpuAccessFlags.Read,
-                BindFlags = BindFlags.None,
-                Format = Format.NV12,
-                Width = Width,
-                Height = Height,
-                OptionFlags = ResourceOptionFlags.None,
-                MipLevels = 1,
-                ArraySize = 1,
-                SampleDescription = { Count = 1, Quality = 0 },
-                Usage = ResourceUsage.Staging
-            });
-
-            _outputSample = MediaFactory.CreateVideoSampleFromSurface(null);
-
-            _outputSample.SampleDuration = 1;
-
-            MediaFactory.CreateDXGISurfaceBuffer(typeof(Texture2D).GUID, _outputTexture, 0, false, out _outBuffer);
-
-            using (var buffer2D = _outBuffer.QueryInterface<Buffer2D>())
-                _outBuffer.CurrentLength = buffer2D.ContiguousLength;
-
-            _outputSample.AddBuffer(_outBuffer);
-
-            _outDataBuffer = new TOutputDataBuffer[1];
-            _outDataBuffer[0].PSample = _outputSample;
-            MediaFactory.CreateCollection(out _outDataBuffer[0].PEvents);
         }
 
         int _frameNumber;
 
-        Texture2D _copyTexture, _outputTexture;
-        Sample _inputSample, _outputSample;
-        MediaBuffer _outBuffer;
-        TOutputDataBuffer[] _outDataBuffer;
+        Texture2D _copyTexture;
+        Sample _inputSample;
 
         public void Convert(Texture2D Texture, byte[] Output)
         {
             Texture.Device.ImmediateContext.CopyResource(Texture, _copyTexture);
 
-            if (_frameNumber == 0)
-            {
-                _inputSample.Set(SampleAttributeKeys.Discontinuity, true);
-            }
-
             _inputSample.SampleTime = _frameNumber++ * 1_000_000;
 
             _colorConverter.ProcessInput(0, _inputSample, 0);
 
-            _colorConverter.ProcessOutput(0, _outDataBuffer, out _);
+            var buf = new MFTOutputDataBuffer[1];
 
-            var ptr = _outBuffer.Lock(out _, out _);
+            // HACK: Need to use Media Foundation .NET here due to bug in SharpDX.MediaFoundation (no longer maintained).
+            // I think the output data buffer is not [Out] marshalled.
+            ((IMFTransform)Marshal.GetObjectForIUnknown(_colorConverter.NativePointer)).ProcessOutput(0, 1, buf, out _);
 
-            Marshal.Copy(ptr, Output, 0, Output.Length);
+            using (var sample = new Sample(buf[0].pSample))
+            {
+                using (var buffer = sample.GetBufferByIndex(0))
+                {
+                    var ptr = buffer.Lock(out _, out _);
 
-            _outBuffer.Unlock();
+                    Marshal.Copy(ptr, Output, 0, Output.Length);
+
+                    buffer.Unlock();
+                }
+            }
         }
 
         public void Dispose()
         {
-            _inputSample.Dispose();
-            _outputSample.Dispose();
+            _colorConverter.ProcessMessage(TMessageType.NotifyEndOfStream, IntPtr.Zero);
 
-            _outBuffer.Dispose();
+            _inputSample.Dispose();
 
             _copyTexture.Dispose();
-            _outputTexture.Dispose();
 
             _colorConverter.Dispose();
             _colorConverter = null;
